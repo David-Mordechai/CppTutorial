21a55da3919f1f4569d7be45e9a32eb1bfb8e5da
David Mordechai <david.mordechai79@gmail.com>
2023-08-04 17:15:10 +0300

    refactoring

--- Demo.Console/main.cpp
+++ Demo.Console/main.cpp
@@ -1,40 +1,7 @@
-//#include <iostream>
-//#include <vector>
 #include <GLFW/glfw3.h>
-//
-//#include "CopyingAndCopyConstructors.h"
-
-//struct Vertex
-//{
-//    float x, y, z;
-//
-//    Vertex(const float x, const float y, const float z)
-//	    : x(x), y(y), z(z)
-//    {
-//	    
-//    }
-//
-//    Vertex(const Vertex& vertex)
-//	    : x(vertex.x), y(vertex.y), z(vertex.z)
-//    {
-//        std::cout << "Copied!" << std::endl;
-//    }
-//};
 
 int main()
 {
-    //CopyingAndCopyConstructors();
-
-    //std::vector<Vertex> vertices;
-    //vertices.reserve(3);
-    //vertices.emplace_back(1, 2, 3);
-    //vertices.emplace_back(4, 5, 6);
-    //vertices.emplace_back(7, 8, 9);
-    //vertices.emplace_back(7, 8, 9);
-    //vertices.emplace_back(7, 8, 9);
-    //std::cin.get();
-
-
     /* Initialize the library */
     if (!glfwInit())
         return -1;
