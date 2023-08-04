72acb2c1e64626e6f2e5b6a32f0d668f5d3fc852
David Mordechai <david.mordechai79@gmail.com>
2023-08-04 17:58:29 +0300

    macro example

--- Demo.Console/main.cpp
+++ Demo.Console/main.cpp
@@ -3,11 +3,18 @@
 #include "CopyingAndCopyConstructors.h"
 #include "VectorOptimization.h"
 
+#ifdef _DEBUG
+// macro example, compiler preprocess
+#define LOG(x) std::cout << (x) << std::endl
+#else
+// the LOG will be removed at compilation time in release mode
+#define LOG(x)
+#endif
 
 int main()
 {
     CopyingAndCopyConstructors();
     VectorOptimization();
 
-	std::cin.get();
+    LOG("Hello");
 }
