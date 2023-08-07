1d26b3c25ba78b42aee6e54ed7097481a7b1e5ef
David Mordechai <david.mordechai79@gmail.com>
2023-08-07 12:14:19 +0300

    two dimensional array vs one dimensional array performance difference

--- Demo.Console/main.cpp
+++ Demo.Console/main.cpp
@@ -1,43 +1,63 @@
-#include <functional>
+#include <chrono>
 #include <iostream>
+#include <thread>
 
-#include "CopyingAndCopyConstructors.h"
-#include "VectorOptimization.h"
-
-#ifdef _DEBUG
-// macro example, compiler preprocess
-#define LOG(x) std::cout << (x) << std::endl
-#else
-// the LOG will be removed at compilation time in release mode
-#define LOG(x)
-#endif
-
-namespace func_pointers
+struct timer
 {
-    void print_value(const int value)
-    {
-        std::cout << "Value with PrintValue function: " << value << std::endl;
-    }
+private:
+	std::string name_;
+public:
+	std::chrono::time_point<std::chrono::steady_clock> start, end;
+	std::chrono::duration<float> duration;
 
-    void my_for_each(const std::vector<int>& values, const std::function<void(int)>& func)
-    {
-        for (const int value : values)
-            func(value);
-    }
-}
+	explicit timer(std::string name) : name_(std::move(name)), duration(0.0f)
+	{
+		start = std::chrono::high_resolution_clock::now();
+	}
+
+	~timer()
+	{
+		end = std::chrono::high_resolution_clock::now();
+		duration = end - start;
+		const float ms = duration.count() * 1000.0f;
+		std::cout << name_ << " - timer took: " << ms << "ms" << std::endl;
+	}
+};
 
 int main()
 {
-    CopyingAndCopyConstructors();
-    VectorOptimization();
-    LOG("Hello");
+	constexpr int dimension_size = 10000;
 
-    const std::vector<int> values = { 1,5,4,2,3 };
-    func_pointers::my_for_each(values, func_pointers::print_value);
+	// scope 1
+	{
+		timer timer("two dimensional array");
+		const auto a2d = new int* [dimension_size];
 
-    int b = 1;
-    func_pointers::my_for_each(values, [b](const int value)
-    {
-	    std::cout << "Value with lambda: " << value + b << std::endl;
-    });
+		for (int i = 0; i < dimension_size; i++)
+			a2d[i] = new int[dimension_size];
+	
+
+		for (int y = 0; y < dimension_size; y++)
+			for (int x = 0; x < dimension_size; x++)
+				a2d[x][y] = 2;
+	
+		for (int i = 0; i < dimension_size; i++)
+			delete[] a2d[i];
+		delete[] a2d;
+		std::cout << "two dimensional array " << dimension_size << " x " << dimension_size << std::endl;
+	}
+
+	// scope 2
+	{
+		timer timer("one dimensional array");
+		int array_size = dimension_size * dimension_size;
+		int* arr = new int[array_size];
+
+		for (int y = 0; y < dimension_size; y++)
+			for (int x = 0; x < dimension_size; x++)
+				arr[x + y * dimension_size] = 2;
+		
+		delete[] arr;
+		std::cout << "one dimensional array " << dimension_size << " * " << dimension_size << std::endl;
+	}
 }
