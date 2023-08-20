98cfdaae7deb88f400d899f56aa58aba24bfb047
David Mordechai <david.mordechai79@gmail.com>
2023-08-20 11:12:02 +0300

    data structure examples

--- Demo.Console/main.cpp
+++ Demo.Console/main.cpp
@@ -1,55 +1,120 @@
-#include <future>
 #include <iostream>
 
-#include "Instrumentor.h"
-
-// chrome://tracing/ => results.json
-
-#define PROFILING 1
-#if PROFILING
-#define PROFILE_SCOPE(name) instrumentation_timer timer##__LINE__(name)
-#define PROFILE_FUNCTION() PROFILE_SCOPE(__FUNCSIG__)
-#else
-#define PROFILE_FUNCTION()
-#endif
-
-void function_1(const int value)
+template<typename T, size_t S>
+class stack_array
 {
-	PROFILE_FUNCTION();
-	std::this_thread::sleep_for(std::chrono::milliseconds(3000));
-	std::cout << "function_1 finished, value: " << value << std::endl;
-}
+public:
+	constexpr size_t size() { return S; }
 
-void function_2()
+	T& operator[](size_t index) { return data_[index]; }
+	const T& operator[](size_t index) const { return data_[index]; }
+
+	T* data() { return data_; }
+	[[nodiscard]] const T* data() const { return data_; }
+
+private:
+	T data_[S];
+};
+
+// writing custom vector class - dynamic array
+template<typename T>
+class my_vector
 {
-	PROFILE_FUNCTION();
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-	std::cout << "function_2 finished\n";
-}
+public:
+	my_vector()
+	{
+		// allocate memory for 2 elements
+		re_alloc(2);
+	}
 
-void function_3()
+	void push_back(const T& value)
+	{
+		if(size_ >= capacity_)
+		{
+			// increase capacity by half
+			re_alloc(capacity_ + capacity_ / 2);
+		}
+
+		data_[size_] = value;
+		size_++;
+	}
+
+	T& operator[](size_t index) const
+	{
+		return data_[index];
+	}
+
+	T& operator[](size_t index)
+	{
+		return data_[index];
+	}
+
+	[[nodiscard]] size_t size() const { return size_; }
+private:
+	void re_alloc(const size_t new_capacity)
+	{
+		// 1. allocate a new block of memory
+		// 2. copy/move old elements into a new block
+		// 3. delete
+
+		T* new_block = new T[new_capacity];
+
+		if (new_capacity < size_)
+			size_ = new_capacity;
+
+		for (size_t i = 0; i < size_; i++)
+			new_block[i] = data_[i];
+
+		delete[] data_;
+		data_ = new_block;
+		capacity_ = new_capacity;
+	}
+private:
+	T* data_ = nullptr;
+
+	size_t size_ = 0;
+	size_t capacity_ = 0;
+};
+
+template<typename T>
+void print_vector(const my_vector<T>& vector)
 {
-	PROFILE_FUNCTION();
-	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-	std::cout << "function_3 finished\n";
-}
+	for (size_t i = 0; i < vector.size(); ++i)
+		std::cout << vector[i] << std::endl;
 
-void run_benchmarks()
-{
-	PROFILE_FUNCTION();
-	std::cout << "Running Benchmarks...\n";
-	std::thread thread1([] {function_1(2); });
-	std::thread thread2([] {function_3(); });
-	std::thread thread3([] {function_2(); });
-
-	thread1.join();
-	thread2.join();
-	thread3.join();
+	std::cout << "----------------------------------\n";
 }
 
 int main()
 {
-	instrumentor::get().begin_session("Profile");
-	run_benchmarks();
-	instrumentor::get().end_session();
+	constexpr int size = 5;
+	stack_array<int, size> data;
+	std::cout << "data size: " << data.size() << std::endl;
+	data[0] = 1;
+	std::cout  << data[0] << std::endl;
+
+	const auto& array_reference = data;
+	for (size_t i = 0; i < data.size(); ++i)
+	{
+		data[i] = 2;
+		std::cout << "data size: " << array_reference[i] << std::endl;
+	}
+
+	memset(data.data(), 0, data.size() * sizeof(int));
+	data[1] = 5;
+	data[3] = 8;
+	for (size_t i = 0; i < data.size(); ++i)
+	{
+		std::cout << "data  " << data[i] << std::endl;
+	}
+
+	std::cout << "\n my custom vector\n----------------------------------\n";
+
+	// my vector implementation
+	my_vector<std::string> vector;
+	vector.push_back("David");
+	vector.push_back("C++");
+	vector.push_back("Vector");
+
+	print_vector(vector);
 }
